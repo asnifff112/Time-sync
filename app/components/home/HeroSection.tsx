817576f7a7a6e6bd0eb29d5af2796efeab6b7ae5
asnif <asnifkl@gmail.com>
2026-01-21 12:37:53 +0530

    ree

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import TextType from "@/effects/TextType"; 
+import TextType from "@/app/effects/TextType"; 
 
 export default function HeroSection() {
   const titleRef = useRef<HTMLHeadingElement>(null);
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from '@/app/components/home/HeroSection'
 import MechanicsOfTime from '@/app/components/home/MechanicsOfTime'
+import TimeInMotion from '@/app/components/home/TimeInMotion'
 // Import CTASection, ScrollWatcher, StorySection similarly
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <HeroSection />
       <MechanicsOfTime />
+      <TimeInMotion />
       {/* Add CTAButtons, ScrollWatcher for pinning, StorySection with text reveals */}
     </>
   )
